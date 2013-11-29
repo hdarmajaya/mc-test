00c99c2404484ed05f94731845ce70515b9eb701
Henry Darmajaya <hdarmajaya@atni.com>
2013-11-29 01:37:32 -0500

    Change find_by_sql

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,32 +9,30 @@ class DashboardController < ApplicationController
   end
 
   def get_counters_by_day(from, to)
-    #GgsnCounter.where(calltime: (from..to)).group("DATE(calltime)").sum(:records_in)
-    GgsnCounter.find_by_sql(["select date(calltime) as calldate, 
-                                     sum(records_in) as sum_records_in,
-                                     sum(records_out) as sum_records_out,
-                                     sum(malformed_data) as sum_malformed_data,
-                                     sum(missing_data) as sum_missing_data,
-                                     sum(zero_value) as sum_zero_value,
-                                     sum(min_too_low) as sum_min_too_low
-                                from ggsn_counters 
-                               where calltime BETWEEN ? AND ?
-                            group by date(calltime)", from, to+1])
+    GgsnCounter.select("date(calltime) as calldate,
+                        sum(records_in) as sum_records_in,
+                        sum(records_out) as sum_records_out,
+                        sum(malformed_data) as sum_malformed_data,
+                        sum(missing_data) as sum_missing_data,
+                        sum(zero_value) as sum_zero_value,
+                        sum(min_too_low) as sum_min_too_low")
+               .where(calltime: (from..to+1))
+               .group("date(calltime)")
+               .order("date(calltime)")
   end
 
   def get_counters_by_hour(from, to)
-    GgsnCounter.find_by_sql(["select date(calltime) as calldate,
-                                     strftime('%H', calltime) as hour,
-                                     sum(records_in) as sum_records_in,
-                                     sum(records_out) as sum_records_out,
-                                     sum(malformed_data) as sum_malformed_data,
-                                     sum(missing_data) as sum_missing_data,
-                                     sum(zero_value) as sum_zero_value,
-                                     sum(min_too_low) as sum_min_too_low
-                                from ggsn_counters
-                               where calltime BETWEEN ? AND ?
-                            group by date(calltime),
-                                     strftime('%H', calltime)", from, to+1])
+    GgsnCounter.select("date(calltime) as calldate,
+                        strftime('%H', calltime) as hour,
+                        sum(records_in) as sum_records_in,
+                        sum(records_out) as sum_records_out,
+                        sum(malformed_data) as sum_malformed_data,
+                        sum(missing_data) as sum_missing_data,
+                        sum(zero_value) as sum_zero_value,
+                        sum(min_too_low) as sum_min_too_low")
+               .where(calltime: (from..to+1))
+               .group("date(calltime)", "strftime('%H', calltime)")
+               .order("date(calltime)", "strftime('%H', calltime)")
   end
 
 end
