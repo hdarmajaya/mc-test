6abe955cebbf11b9de638d38f12bc00faddcfeb6
Henry Darmajaya <hdarmajaya@atni.com>
2013-12-02 15:04:18 -0500

    Add flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when :notice then "alert-info"
+    when :success then "alert-success"
+    when :error then "alert-danger"
+    when :alert then "alert-warning"
+    end
+  end
 
   def display_date(date)
     return Date.parse(date).strftime('%a, %d %b %Y')
