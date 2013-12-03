e1ec47845aeb5b428865860a04ccc9850ca7a152
Henry Darmajaya <hdarmajaya@atni.com>
2013-12-03 14:32:08 -0500

    Reformat code

--- app/models/ggsn_counter.rb
+++ app/models/ggsn_counter.rb
@@ -35,13 +35,13 @@ class GgsnCounter < ActiveRecord::Base
 
   def self.by_hour(from, to)
     select("date(calltime) as calldate,
-             strftime('%H', calltime) as hour,
-             sum(records_in) as sum_records_in,
-             sum(records_out) as sum_records_out,
-             sum(malformed_data) as sum_malformed_data,
-             sum(missing_data) as sum_missing_data,
-             sum(zero_value) as sum_zero_value,
-             sum(min_too_low) as sum_min_too_low")
+            strftime('%H', calltime) as hour,
+            sum(records_in) as sum_records_in,
+            sum(records_out) as sum_records_out,
+            sum(malformed_data) as sum_malformed_data,
+            sum(missing_data) as sum_missing_data,
+            sum(zero_value) as sum_zero_value,
+            sum(min_too_low) as sum_min_too_low")
     .where(calltime: (from..to+1))
     .group("date(calltime)", "strftime('%H', calltime)")
     .order("date(calltime)", "strftime('%H', calltime)")
@@ -49,14 +49,14 @@ class GgsnCounter < ActiveRecord::Base
 
   def self.by_file(from, to)
     select("date(calltime) as calldate,
-             strftime('%H', calltime) as hour,
-             filename as file,
-             sum(records_in) as sum_records_in,
-             sum(records_out) as sum_records_out,
-             sum(malformed_data) as sum_malformed_data,
-             sum(missing_data) as sum_missing_data,
-             sum(zero_value) as sum_zero_value,
-             sum(min_too_low) as sum_min_too_low")
+            strftime('%H', calltime) as hour,
+            filename as file,
+            sum(records_in) as sum_records_in,
+            sum(records_out) as sum_records_out,
+            sum(malformed_data) as sum_malformed_data,
+            sum(missing_data) as sum_missing_data,
+            sum(zero_value) as sum_zero_value,
+            sum(min_too_low) as sum_min_too_low")
     .where(calltime: (from..to+1))
     .group("date(calltime)", "strftime('%H', calltime)", :filename)
     .order("date(calltime)", "strftime('%H', calltime)", :filename)
